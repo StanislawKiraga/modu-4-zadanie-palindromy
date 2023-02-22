48ecd0867da56083f343710fb3089c1108aa5b4d
StanislawKiraga <kiragasta@gmail.com>
2023-02-22 11:58:11 +0100

    check funtion working

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ def is_palindrom(word: str) -> bool:
     word = word.replace(' ', '')
     word = word.lower()
 
+    return word[::-1] == word
     
+words = ['kajak', 'Poto p', 'stachu']
+for word in words:
+    print(is_palindrom(word))
