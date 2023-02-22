f204c44a1fd4d99a9c74013dfe14a1ca706bdfde
StanislawKiraga <kiragasta@gmail.com>
2023-02-22 12:01:35 +0100

    add some coments to function

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def is_palindrom(word: str) -> bool:
+    #najpierw usuwam spacje ze stringa
     word = word.replace(' ', '')
+    #i zmieniam wszystkie znaki na małe
     word = word.lower()
-
+    #teraz porównuję czy słowo wygląda tak samo od lewej do prawej i na odwrót
     return word[::-1] == word
     
 words = ['kajak', 'Poto p', 'stachu']
