a1c4f452f18c7bf5bee40050e532ed2725212600
StanislawKiraga <kiragasta@gmail.com>
2023-02-22 12:51:47 +0100

    delete special signs from word

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ def is_palindrom(word: str) -> bool:
     word = word.replace(' ', '')
     #i zmieniam wszystkie znaki na małe
     word = word.lower()
-    #teraz porównuję czy słowo wygląda tak samo od lewej do prawej i na odwrót
+    #usuwam jeszcze znaki specjane ze stringa
     word = ''.join(filter(str.isalnum, word))
+    #teraz porównuję czy słowo wygląda tak samo od lewej do prawej i na odwrót
     return word[::-1] == word
     
 words = ['kajak', 'Poto p', 'stachu', 'kobyła, ma. mały! bok?']
